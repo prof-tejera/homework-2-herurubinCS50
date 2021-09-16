a5f5b4f09316e4e63d58c44f2539c150355096ac
heru rubin <herurubin@herus-Mac-mini.local>
2021-09-16 06:12:14 -0400

    pages md added

---
+++ my-first-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello CSCI E-39!</p>
+        <p>
+          My name is Heru
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
